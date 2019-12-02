b9f2f755ea72e128acd29f64d936503d47237886
carmurio <carmurio@gmail.com>
2019-12-01 23:33:33 -0500

    Spring Initializr Java Project just created

---
+++ src/test/java/com/carmuriolabs/myportfolio/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmuriolabs.myportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
