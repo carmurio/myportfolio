00f9bacd935fda4a38b19af5c59b043c0c88069d
carmurio <carmurio@gmail.com>
2019-12-01 23:39:32 -0500

    Renamed to MyPortfolio

--- src/main/java/com/carmuriolabs/myportfolio/MyPortfolio.java
+++ src/main/java/com/carmuriolabs/myportfolio/MyPortfolio.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MyPortfolio {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MyPortfolio.class, args);
 	}
 
 }
